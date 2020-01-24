bb9082105c2770ade3393e12be4222a60de8b9da
Michelle Ranly <meranly8@gmail.com>
2020-01-24 19:00:06 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -85,4 +85,16 @@ class TicTacToe
     end
   end
   
+  def play 
+    while !over?
+      turn
+    end
+    
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif
+      puts "Cat's Game!"
+    end
+  end
+  
 end
